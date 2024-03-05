c065bcf5bc39d90073a7abcb19f37107802e073a
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2024-03-05 22:34:36 +0530

    Create Day01.py

---
+++ Day01.py
@@ -0,0 +1,6 @@
+ch=input("Enter a character:")
+c=ch.upper()
+if c=="A" or c=="E" or c=="I" or c=="O" or c=="U":
+    print("The given character is an Vowel.")
+else:
+    print("The given character is a Consonant.")
