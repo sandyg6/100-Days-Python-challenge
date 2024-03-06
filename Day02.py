a77f1d1b3ab4544feb224d586cecf5bbc88ce1b8
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2024-03-06 19:11:34 +0530

    Create Day02.py

---
+++ Day02.py
@@ -0,0 +1,5 @@
+alpha = ord(input("Enter a character:"))
+if 65<=alpha<=90 or 97<=alpha<=122:
+    print("The given character is an alphabet")
+else:
+    print("The given character is not an alphabet")
