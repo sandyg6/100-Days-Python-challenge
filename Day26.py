e479364c63fad105a6099bff59028e6448673f3c
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2024-03-30 21:25:07 +0530

    Update Day26.py

--- Day26.py
+++ Day26.py
@@ -1,8 +1,9 @@
 leap= int(input("Enter a year:"))
-if leap%4==0:
-    if leap%100==0:
-        print(f"{leap} is a leap year")
-    elif leap%400!=0:
-        print(f"{leap} is not a leap year")
+if leap%400==0:
+    print(f"{leap} is a leap year")
+elif leap%100==0:
+    print(f"{leap} is not a leap year")
+elif leap%4==0:
+    print(f"{leap} is a leap year")
 else:
     print(f"{leap} is not a leap year")
